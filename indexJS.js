a341066b9e7aa2247aa4215663f64034f3c9f9bd
tylerae <84347788+tylerae@users.noreply.github.com>
2023-12-05 23:21:34 -0700

    Update indexJS.js

--- indexJS.js
+++ indexJS.js
@@ -8,3 +8,26 @@
         }).find('.skip-link').append('<span>'+$('#menu .active').text()+'</span>');
     });
 })(jQuery);
+
+let slideIndex = 0;
+
+function showSlides() {
+let i;
+const slides = document.getElementsByClassName("mySlides");
+
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+
+slideIndex++;
+
+if (slideIndex > slides.length) {
+  slideIndex = 1;
+}
+
+slides[slideIndex - 1].style.display = "block";
+
+setTimeout(showSlides, 3000); // Change slide every 3 seconds (adjust as needed)
+}
+
+showSlides();
